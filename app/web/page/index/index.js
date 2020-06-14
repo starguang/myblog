3506535907b0954ac01c764f9a89db8359bf5d9e
596094101 <596094101@qq.com>
2020-06-14 18:49:24 +0800

    start

---
+++ app/web/page/index/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './index.less'
+
+function Page (props) {
+  return (
+    <div className='normal'>
+      <div className='welcome' />
+      <ul className='list'>
+        {
+          props.news && props.news.map(item => (
+            <li key={item.id}>
+              <div>文章标题: {item.title}</div>
+              <div className='toDetail'><Link to={`/news/${item.id}`}>点击查看详情</Link></div>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+Page.getInitialProps = async (ctx) => {
+  // ssr渲染模式只在服务端通过Node获取数据，csr渲染模式只在客户端通过http请求获取数据，getInitialProps方法在整个页面生命周期只会执行一次
+  return __isBrowser__ ? (await window.fetch('/api/getIndexData')).json() : ctx.service.api.index()
+}
+
+export default Page
