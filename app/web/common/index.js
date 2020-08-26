4a3dd83cd8e464058f062222373ddd104382deae
赵晨光 <zcg@51box.cn>
2020-08-26 15:40:20 +0800

    内容顺序整理

--- app/web/common/index.js
+++ app/web/common/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter, StaticRouter, Route, Switch } from 'react-router-dom';
 import './index.less';
 
 export const blonList = (filesArr, props) => {
-  return filesArr.reverse().map((file) => {
+  return filesArr.map((file) => {
     return <p className="file_list" onClick={() => {
       props.history.push(`${props.location.pathname}/detail?fileName=${file}`)
     }} key={file}>{file}</p>
